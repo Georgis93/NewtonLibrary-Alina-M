9933dd014540ca4c294d3e2ccb5834f34bca1f1e
Georgis93 <georgiana.alina93@yahoo.com>
2023-12-04 17:54:30 +0100

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton_Bibliotek_Alina.Models
+{
+    internal class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public Author()
+        {
+            
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton_Bibliotek_Alina.Models
+{
+    internal class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int RealiseYear { get; set; }
+        public Book()
+        {
+            
+        }
+    }
+}
